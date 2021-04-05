3c2c16b3faa2b160254216be86acb717efe2c0a7
NoOneZero <TURDECARTA@gmail.com>
2021-04-06 01:46:34 +0300

    Додано клас 1 нейрон

--- common/neuron.py
+++ common/neuron.py
@@ -1,22 +1,51 @@
 class Neuron:
-    def __init__(self, fathers: list =[], children: list =[], bias: int=0, activation_funk=lambda x:x):
-        self.fathers = fathers
-        self.children = children
-        self.bias = bias
+    def __init__(self, fathers: list =[], bias_weigh: int=0, activation_funk=lambda x:x):
+        self.fathers_relation = [Relation(fathers[i]) for i in range(len(fathers))]
+        self.bias_weigh = bias_weigh
         self.result = 0
         self.activation_funk = activation_funk
 
-    def add_father(self, fathers=[]): self.fathers.extend(fathers)
-    def add_children(self, children=[]): self.children.extend(children)
+    def copy(self):
+        neuron = Neuron(fathers=[], bias_weigh=self.bias_weigh, activation_funk=self.activation_funk)
+        for i in range(len(self.fathers_relation)):
+            neuron.fathers_relation.append(self.fathers_relation)
+        return neuron
+    def copy_no_weigh(self):
+        fathers = [self.fathers_relation[i].get_direction() for i in range(len(self.fathers_relation))]
+        neuron = Neuron(fathers=fathers, bias_weigh=self.bias_weigh, activation_funk=self.activation_funk)
+        return neuron
+
+    def add_father(self, fathers: list=[]):
+        for i in range(len(fathers)):
+            self.fathers_relation.append(Relation(fathers[i], 0))
     def get_result(self): return self.result
+
+
+    def set_result(self, value):self.result = value
+
 
     def calculate_result(self):
         suma = self.sumator()
         return self.activation(suma)
     def sumator(self):
-        suma = 0
-        for i in range(len(self.fathers)):
-            suma += self.fathers[i].get_result()
+        suma = self.bias_weigh
+        for i in range(len(self.fathers_relation)):
+            suma += self.fathers_relation[i].get_multiply()
         return suma
     def activation(self, number):
-        return self.activation_funk(number)
+        self.result = self.activation_funk(number)
+        return self.result
+
+class Relation:
+    def __init__(self, direction: Neuron, weigh=0):
+        self.direction = direction
+        self.weigh = weigh
+
+    def get_multiply(self): return self.weigh * self.direction.get_result()
+    def get_direction(self): return self.direction
+    def get_weigh(self): return self.weigh
+    def set_direction(self, direction): self.direction = direction
+    def set_weigh(self, weigh): self.weigh = weigh
+
+    def copy(self): return Relation(self.direction, self.weigh)
+    def copy_no_weigh(self, weigh=0): return Relation(self.direction, weigh=weigh)
